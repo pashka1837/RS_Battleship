b9d218beee48bd7f5477f44d4db7c446a9cd3623
pashka1837 <pashka1837@yandex.kz>
2024-02-25 20:37:18 +0600

    fix: winner controller won't show bot's profile; feat: readme updated

--- src/web_server/controllers/winner.ts
+++ src/web_server/controllers/winner.ts
@@ -4,9 +4,10 @@ import { createResponse } from "../../utils/utils.js";
 export default function winner() {
   const data = [...db.getUsersMap.values()]
     .sort((a, b) => b.wins - a.wins)
+    .filter((user) => !user.name.includes("bot - "))
     .map((user) => ({ name: user.name, wins: user.wins }));
   const response = createResponse("update_winners", data);
-  [...db.ws_users_Map.keys()].forEach((ws) => {
-    ws.send(response);
+  db.ws_users_Map.forEach((user, ws) => {
+    if (!user.name.includes("bot - ")) ws.send(response);
   });
 }
